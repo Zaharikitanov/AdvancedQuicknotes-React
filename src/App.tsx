e89441bc1a7f4d44fdc8bd86687a5037e4b2d246
Zahari.Kitanov@stp-online.de <Zahari.Kitanov@stp-online.de>
2023-01-06 00:19:49 +0200

    Added columns functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useRef, useState } from "react";
 import Quicknote from "./quicknote";
 import "./styles.css"
 
 export default function App() {
-  
-    return <div className="container">
-      <h1>Hello World!</h1>
-      <div className="row">
-        <Quicknote title={"Test"}/>
+
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [columns, addColumn] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  }, [columns]);
+
+  const AddNewRow = (inputValue: HTMLInputElement | null) => {
+    if (inputValue) {
+      addColumn(prev => [...prev, inputValue.value.toString()])
+    }
+  }
+
+  return <div className="container">
+    <div className="row py-4">
+      <div className='d-flex justify-content-between col-3 mx-3 px-0 py-4'>
+        <input className="input-field p-2 px-3" ref={inputRef} />
+        <button className="red-btn p-2 px-3" onClick={() => AddNewRow(inputRef.current)} >+ Column</button>
       </div>
     </div>
-  }
+    <div className="row">
+      {columns.map((value, index) => {
+        return <Quicknote key={index} title={value} />
+      })}
+    </div>
+  </div>
+}
--- src/quicknote.tsx
+++ src/quicknote.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-export default function Quicknote(title) {
+export default function Quicknote({title} : {title:string}) {
 
     const inputRef = useRef<HTMLInputElement>(null)
     const [rows, addRow] = useState<string[]>([]);
@@ -18,16 +18,16 @@ export default function Quicknote(title) {
     }
 
     return <>
-        <div className='quicknote col-3'>
-            <h1>{title}</h1>
-            <div className='note-items'>
+        <div className='quicknote col-3 m-3'>
+            <h2>{title}</h2>
+            <div className='note-items pt-4'>
                 {rows.map((value, index) => {
                     return <p key={index}>{index + 1}. {value}</p>
                 })}
             </div>
-            <div className='d-flex justify-content-center pt-3'>
-                <input className="p-2 px-3" ref={inputRef} />
-                <button className="p-2 px-3" onClick={() => AddNewRow(inputRef.current)} >+ Row</button>
+            <div className='d-flex justify-content-between pt-3'>
+                <input className="input-field p-2 px-3" ref={inputRef} />
+                <button className="red-btn p-2 px-3" onClick={() => AddNewRow(inputRef.current)} >+ Row</button>
             </div>
         </div>
     </>
